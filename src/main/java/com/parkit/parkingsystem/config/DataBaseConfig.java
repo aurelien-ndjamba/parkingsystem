bfd2848614f764e830d4cdf4d40bcb2b2ed32a09
andjamba <aurelien.ndjamba@gmail.com>
2021-04-06 10:30:16 +0200

    Suppression de l'ensemble des bugs potentiels suggérés par SPOTBUGS

--- src/main/java/com/parkit/parkingsystem/config/DataBaseConfig.java
+++ src/main/java/com/parkit/parkingsystem/config/DataBaseConfig.java
@@ -2,18 +2,57 @@ package com.parkit.parkingsystem.config;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.*;
+
+import java.util.Properties;
+import org.jasypt.iv.RandomIvGenerator;
+import org.jasypt.properties.EncryptableProperties;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 
 public class DataBaseConfig {
 
     private static final Logger logger = LogManager.getLogger("DataBaseConfig");
 
-    public Connection getConnection() throws ClassNotFoundException, SQLException {
+    public Connection getConnection() throws ClassNotFoundException,  SQLException, SecurityException {
+    	FileInputStream fileInputStream = null;
+    	
+    	StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        encryptor.setPassword("jasypt"); // could be got from web, env variable...
+        encryptor.setAlgorithm("PBEWithHMACSHA512AndAES_256");
+        encryptor.setIvGenerator(new RandomIvGenerator());
+        
+        Properties props = new EncryptableProperties(encryptor);
+        
+    	String datasourceDriver = props.getProperty("datasource.driver");
+        String datasourceUrl = props.getProperty("datasource.url");
+        String datasourceUsername = props.getProperty("datasource.username");
+        String datasourcePassword = props.getProperty("datasource.password");
+        String datasourcePath = props.getProperty("datasource.Path");
+        
+        try {
+        fileInputStream = new FileInputStream(datasourcePath);
+        props.load(fileInputStream);
+        
         logger.info("Create DB connection");
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        return DriverManager.getConnection(
-        		"jdbc:mysql://localhost:3306/prod?zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=UTC","root","rootroot");
+        Class.forName(datasourceDriver);
+        
+    	} catch(IOException ex) {
+    		logger.error("Database connection error",ex);
+    	} finally{
+    		try {
+    			if (fileInputStream != null)
+        			fileInputStream.close();
+    		} catch (IOException ex) {
+    			logger.error("Database connection error",ex);
+    		}
+    		return DriverManager.getConnection(datasourceUrl, datasourceUsername, datasourcePassword);	
+    	}
+        
+        
+        
+//        		"jdbc:mysql://localhost:3306/prod?zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=UTC","root","rootroot");
     }
 
     public void closeConnection(Connection con){
--- src/main/java/com/parkit/parkingsystem/dao/ParkingSpotDAO.java
+++ src/main/java/com/parkit/parkingsystem/dao/ParkingSpotDAO.java
@@ -10,6 +10,8 @@ import org.apache.logging.log4j.Logger;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLTimeoutException;
+import java.sql.SQLException;
 
 public class ParkingSpotDAO {
     private static final Logger logger = LogManager.getLogger("ParkingSpotDAO");
@@ -18,21 +20,28 @@ public class ParkingSpotDAO {
 
     public int getNextAvailableSlot(ParkingType parkingType){
         Connection con = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
         int result=-1;
         try {
             con = dataBaseConfig.getConnection();
-            PreparedStatement ps = con.prepareStatement(DBConstants.GET_NEXT_PARKING_SPOT);
+            ps = con.prepareStatement(DBConstants.GET_NEXT_PARKING_SPOT);
             ps.setString(1, parkingType.toString());
-            ResultSet rs = ps.executeQuery();
+            rs = ps.executeQuery();
             if(rs.next()){
                 result = rs.getInt(1);
             }
-            dataBaseConfig.closeResultSet(rs);
-            dataBaseConfig.closePreparedStatement(ps);
+        }catch (SQLTimeoutException ex){
+            logger.error("Error fetching next available slot",ex);
+        }catch (SQLException ex){
+            logger.error("Error fetching next available slot",ex);
         }catch (Exception ex){
             logger.error("Error fetching next available slot",ex);
         }finally {
+            dataBaseConfig.closeResultSet(rs);
+            dataBaseConfig.closePreparedStatement(ps);
             dataBaseConfig.closeConnection(con);
+            
         }
         return result;
     }
@@ -40,18 +49,20 @@ public class ParkingSpotDAO {
     public boolean updateParking(ParkingSpot parkingSpot){
         //update the availability fo that parking slot
         Connection con = null;
+        PreparedStatement ps = null;
         try {
             con = dataBaseConfig.getConnection();
-            PreparedStatement ps = con.prepareStatement(DBConstants.UPDATE_PARKING_SPOT);
+            ps = con.prepareStatement(DBConstants.UPDATE_PARKING_SPOT);
             ps.setBoolean(1, parkingSpot.isAvailable());
             ps.setInt(2, parkingSpot.getId());
             int updateRowCount = ps.executeUpdate();
-            dataBaseConfig.closePreparedStatement(ps);
+           
             return (updateRowCount == 1);
         }catch (Exception ex){
             logger.error("Error updating parking info",ex);
             return false;
         }finally {
+        	dataBaseConfig.closePreparedStatement(ps);
             dataBaseConfig.closeConnection(con);
         }
     }
